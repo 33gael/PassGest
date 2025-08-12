b04d6989eb3abb66433639b4d343033c736986cd
Gaël <33gael.dev@gmail.com>
2025-08-12 12:37:18 +0200

    remove second choice

--- passgest.js
+++ passgest.js
@@ -8,18 +8,14 @@ const ivd = "Invalid option.";
 function menu() {
   console.log("PassGest".bgBlack.red, "by @33gael".green);
   console.log("1 - Generate a password");
-  console.log("2 - View saved password");
 
   let choix = readlineSync.question("Choose an option : ");
   if (!choix) return;
   choix = parseInt(choix);
-  if (choix > 2) return ivd;
+  if (choix >= 2) return ivd;
   switch (choix) {
     case 1:
       console.log("1 - Generate a password : ");
-      break;
-    case 2:
-      console.log("2 - View saved password : ");
       break;
   }
 }
